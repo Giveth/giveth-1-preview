0649053ef6bd604c0767567e5c36a0a910e8333a
Mahdi <m.dehghani612@gmail.com>
2021-03-08 09:46:06 +0330

    support slug

--- controllers/dac.controller.js
+++ controllers/dac.controller.js
@@ -2,17 +2,37 @@ const DacModel = require('../models/dac.model');
 const htmlUtil = require('../utils/htmlUtil');
 const imageUtil = require('../utils/imageUtil');
 
+function normalize(dac) {
+    dac.description = htmlUtil.getPlainText(dac.description);
+    dac.image = imageUtil.normalizeImagePath(dac.image);
+    return dac;
+}
+
 module.exports = {
-    getDac: async (req, res) => {
+    getDacById: async (req, res) => {
         const id = req.params.id;
         try {
             const dac = await DacModel.findById(id).exec();
             if (!dac) {
                 res.sendStatus(404);
             }
-            dac.description = htmlUtil.getPlainText(dac.description);
-            dac.image = imageUtil.normalizeImagePath(dac.image);
-            res.render('dac', dac);
+            normalize(dac);
+            return res.render('dac', dac);
+        }
+        catch(err) {
+            console.log('err', err);
+            res.sendStatus(500);
+        }
+    },
+    getDacBySlug: async (req, res) => {
+        const slug = req.params.slug;
+        try {
+            const dac = await DacModel.findOne({slug}).exec();
+            if (!dac) {
+                res.sendStatus(404);
+            }
+            normalize(dac);
+            return res.render('dac', dac);
         }
         catch(err) {
             console.log('err', err);
